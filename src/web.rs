49e8f8a667b96a659f7beb63030aece386b5fe63
soeur <juzi201314@gmail.com>
2021-01-18 16:28:22 +0800

    完成web的get功能

--- src/web.rs
+++ src/web.rs
@@ -1,20 +1,22 @@
-use rocket::{Request, Route};
+use rocket::{Request, Route, Response};
 use rocket::data::ByteUnit;
 use rocket::handler::HandlerFuture;
-use rocket::http::{MediaType, Method, Status};
+use rocket::http::{MediaType, Method, Status, RawStr, Header};
 use rocket::outcome::Outcome;
 use rocket::request::Outcome as ReqOutcome;
 use serde::Deserialize;
 use rocket::request::FromRequest;
-use crate::db::{add_short_link, Model};
+use crate::db::{add_short_link, Model, get_short_link};
 use anyhow::Error;
 
 pub async fn listen() -> anyhow::Result<()> {
     let mut add_route = Route::new(Method::Post, "/", add);
     add_route.format = Some(MediaType::JSON);
 
+    let get_route = Route::new(Method::Get, "/<short>", get);
+
     rocket::ignite()
-        .mount("/", vec![add_route])
+        .mount("/", vec![add_route, get_route])
         .launch()
         .await?;
 
@@ -31,6 +33,35 @@ impl ApiKey {
             _ => false
         }
     }
+}
+
+fn get<'r>(req: &'r Request, _: rocket::data::Data) -> HandlerFuture<'r> {
+    Box::pin(async move {
+        let short = req
+            .get_param::<&RawStr>(0)
+            .and_then(|res| res.ok())
+            .expect("链接缺少标识符")
+            .to_string();
+        match get_short_link(short).await {
+            Ok(model) => {
+                if let Some(model) = model {
+                    Outcome::from(req,
+                                  Response::build()
+                                      .status(Status::Ok)
+                                      .header(Header::new("Location", model.link))
+                                      .ok::<()>()
+                                      .unwrap()
+                    )
+                } else {
+                    Outcome::failure(Status::NotFound)
+                }
+            },
+            Err(e) => {
+                error!("get_short_link error: {:?}", e);
+                Outcome::failure(Status::InternalServerError)
+            }
+        }
+    })
 }
 
 #[derive(Deserialize)]
